2eb99ff5d851d73e599e20ac13d5cb73d51c3751
saaid323 <saaid2538@gmail.com>
2023-10-04 20:33:34 +0000

    added 0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# script that sets up your web servers for the deployment of web_static
+if ! dpkg -l | grep -q "nginx"; then
+        sudo apt-get -y update
+        sudo apt-get -y install nginx
+fi
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "<html><head></head><body>Hello, web_static!</body></html>" >  /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+echo "server {
+    listen 80;
+    location /hbnb_static/ {
+        alias /data/web_static/current/;
+        index index.html;
+    }
+}" >  /etc/nginx/sites-available/default
+
+sudo service nginx restart
