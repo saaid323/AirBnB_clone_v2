cb2ddf6039363033bf39e398396f67411cd870c7
saaid323 <saaid2538@gmail.com>
2023-10-05 05:17:43 -0700

    added 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # script that sets up your web servers for the deployment of web_static
-if ! dpkg -l | grep -q "nginx"; then
-        sudo apt-get -y update
-        sudo apt-get -y install nginx
-fi
-mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+
+sudo apt-get -y update
+sudo apt-get -y install nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 echo "<html><head></head><body>Hello, web_static!</body></html>" >  /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
