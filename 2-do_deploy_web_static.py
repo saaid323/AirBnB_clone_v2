c896b31f980b6088fc32bb393a54a0059a4d30cb
saaid323 <saaid2538@gmail.com>
2023-10-05 10:11:24 -0700

    added 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/python3
 """abric script (based on the file 1-pack_web_static.py) that distributes
 an archive to your web servers, using the function do_deploy """
 from fabric.api import run, put, env, local
