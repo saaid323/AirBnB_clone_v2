f8b014b6fc445bf7a2eac4d189cb65671220901b
saaid323 <saaid2538@gmail.com>
2023-10-19 16:36:27 +0000

    added 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application""" 
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """display "C" followed by the value of the text variable"""
+    x = text
+    if '_' in text:
+        x = text.replace('_', ' ')
+    return 'C {}'.format(escape(x))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
