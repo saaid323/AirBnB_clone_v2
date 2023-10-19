0cec7c34c6ea52cf49fe4b8785114d2169d179c7
saaid323 <saaid2538@gmail.com>
2023-10-19 17:18:59 +0000

    added 4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/number/int:<n>', strict_slashes=False)
+def number(n):
+    """display "n is a number" only if n is an integer"""
+    return '{} is a number'.format(escape(n))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
