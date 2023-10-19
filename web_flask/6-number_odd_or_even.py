f40cc9315f7694a3bdd6196a9baf7f70d4f29bc3
saaid323 <saaid2538@gmail.com>
2023-10-19 17:59:30 +0000

    added 6-number_odd_or_even.py, templates/6-number_odd_or_even.html

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
