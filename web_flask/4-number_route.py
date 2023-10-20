5eb6557dd4083afb540f98d68c16edbe8715de9b
saaid323 <saaid2538@gmail.com>
2023-10-20 11:44:38 +0000

    added 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -6,7 +6,7 @@ from markupsafe import escape
 app = Flask(__name__)
 
 
-@app.route('/number/int:<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """display "n is a number" only if n is an integer"""
     return '{} is a number'.format(escape(n))
