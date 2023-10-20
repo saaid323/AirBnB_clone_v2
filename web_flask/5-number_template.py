608b9e59016914e2e6d760d648da72d76a2ff572
saaid323 <saaid2538@gmail.com>
2023-10-20 11:52:22 +0000

    added 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""script that starts a Flask web application"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,6 +7,7 @@ app = Flask(__name__)
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_render(n):
+    """display a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
 
