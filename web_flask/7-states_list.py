1a13e7f76e28b11169ff6612c98d810a5382fce8
saaid323 <saaid2538@gmail.com>
2023-10-21 12:57:42 +0000

    added web_flask/7-states_list.py, web_flask/templates/7-states_list.html

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,18 +2,20 @@
 """script that starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def state():
     """display a HTML page with states listed"""
-    states = storage.all("State").values()
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
+    """remove the current SQLAlchemy Session after each request"""
     storage.close()
 
 
