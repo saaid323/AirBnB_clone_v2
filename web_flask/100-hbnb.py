6a1451b50da9fa403d5a7dcd65ccaa528f010753
saaid323 <saaid2538@gmail.com>
2023-10-22 20:17:25 +0000

    added 10-hbnb_filters.py

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -4,15 +4,18 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.amenity import Amenity
+from models.place import Place
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def all():
-    """displays 10-hbnb_filters.html"""
+    """displays 100-hbnb.html"""
     states = storage.all(State).values()
     amenities = storage.all(Amenity).values()
-    return render_template("10-hbnb_filters.html", amen=amenities, state=states)
+    places = storage.all(Place).values()
+    return render_template("100-hbnb.html", amen=amenities,
+                           state=states, places=places)
 
 
 @app.teardown_appcontext
